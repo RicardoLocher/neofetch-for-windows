ffcda332a6be878e970f434b38070d22ba6364d9
RicardoLocher <ricardo.locher123@gmail.com>
2024-03-10 23:14:26 +0100

    Added Windows Logo to output to make it more look like neofetch

--- neofetch.py
+++ neofetch.py
@@ -3,28 +3,59 @@ import psutil
 cpu = psutil.cpu_percent(interval=1)
 cpu_freq = psutil.cpu_freq()
 
-print("\nCPU Information:")
-print(f"CPU usage:      {cpu}%")
-print(f"CPU frequency:  {cpu_freq.current}MHz")
-print("######################################")
+output = [
+    "CPU Information:                       ",
+    f"CPU usage:      {cpu}%                ",
+    f"CPU frequency:  {cpu_freq.current}MHz ",
 
-print("\nMemory Information:")
-print(f"Total:          {round(psutil.virtual_memory().total / 1024 / 1024, 2)}MB")
-print(f"Used:           {round(psutil.virtual_memory().used / 1024 / 1024, 2)}MB")
-print(f"Available:      {round(psutil.virtual_memory().available / 1024 / 1024, 2)}MB")
-print(f"Percentage:     {round(psutil.virtual_memory().percent, 2)}%")
-print("######################################")
+    "Memory Information:                    ",
+    f"Total:          {round(psutil.virtual_memory().total / 1024 / 1024, 2)}MB",
+    f"Used:           {round(psutil.virtual_memory().used / 1024 / 1024, 2)}MB",
+    f"Available:      {round(psutil.virtual_memory().available / 1024 / 1024, 2)}MB",
+    f"Percentage:     {round(psutil.virtual_memory().percent, 2)}%",
 
-print("\nDisk Information:")
-print(f"Total:          {round(psutil.disk_usage('/').total / 1024 / 1024 / 1024, 2)}GB")
-print(f"Used:           {round(psutil.disk_usage('/').used / 1024 / 1024 / 1024, 2)}GB")
-print(f"Free:           {round(psutil.disk_usage('/').free / 1024 / 1024 / 1024, 2)}GB")
-print(f"Percentage:     {round(psutil.disk_usage('/').percent, 2)}%")
-print("######################################")
+    "Disk Information:                      ",
+    f"Total:          {round(psutil.disk_usage('/').total / 1024 / 1024 / 1024, 2)}GB",
+    f"Used:           {round(psutil.disk_usage('/').used / 1024 / 1024 / 1024, 2)}GB",
+    f"Free:           {round(psutil.disk_usage('/').free / 1024 / 1024 / 1024, 2)}GB",
+    f"Percentage:     {round(psutil.disk_usage('/').percent, 2)}%",
 
-print("\nNetwork Information:")
-print(f"Total Bytes Sent:   {round(psutil.net_io_counters().bytes_sent / 1024 / 1024, 2)}MB")
-print(f"Total Bytes Recv:   {round(psutil.net_io_counters().bytes_recv / 1024 / 1024, 2)}MB")
-print(f"Total Packets Sent: {psutil.net_io_counters().packets_sent}")
-print(f"Total Packets Recv: {psutil.net_io_counters().packets_recv}")
-print("######################################")
+    "Network Information:                   ",
+    f"Total Bytes Sent:   {round(psutil.net_io_counters().bytes_sent / 1024 / 1024, 2)}MB",
+    f"Total Bytes Recv:   {round(psutil.net_io_counters().bytes_recv / 1024 / 1024, 2)}MB",
+    f"Total Packets Sent: {psutil.net_io_counters().packets_sent}",
+    f"Total Packets Recv: {psutil.net_io_counters().packets_recv}",
+]
+
+# Implementation of the Windows Logo to be displayed on the console
+
+logo = [
+"                             .oodMMMM",
+"                    .oodMMMMMMMMMMMMM",
+"        ..oodMMM  MMMMMMMMMMMMMMMMMMM",
+"  oodMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+"  MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+"  MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+"  MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+"  MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+"  MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+"                                     ",
+"  MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+"  MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+"  MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+"  MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+"  MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+"  `^^^^^^MMMMMMM  MMMMMMMMMMMMMMMMMMM",
+"        ````^^^^  ^^MMMMMMMMMMMMMMMMM",
+"                       ````^^^^^^MMMM",
+]
+
+# printing the output
+# the useful output should be in an array to join them together more easily
+
+print("\n\n\n")
+
+for i in range(len(logo)):
+    print(logo[i] + "\t\t" + output[i])
+
+print("\n\n\n")
